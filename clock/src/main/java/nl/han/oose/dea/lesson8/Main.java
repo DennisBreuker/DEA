b195cd38c0599f60772f615e6f76c4fb6fbe0cef
Dennis Breuker <dennis@dennisbreuker.nl>
2018-03-01 11:24:37 +0100

    Added superclass ConsoleView, restructured app, adjusted 12-hr clock

--- clock/src/main/java/nl/han/oose/dea/lesson8/Main.java
+++ clock/src/main/java/nl/han/oose/dea/lesson8/Main.java
@@ -1,21 +1,16 @@
 package nl.han.oose.dea.lesson8;
 
+import nl.han.oose.dea.lesson8.model.Clock;
+import nl.han.oose.dea.lesson8.view.TwelveHour;
+import nl.han.oose.dea.lesson8.view.TwentyFourHour;
+
 public class Main {
     public static void main(String[] args) {
-        Clock clock = new Clock();
+        Clock clock = new Clock(23, 59, 58);
         clock.addObserver(new TwelveHour());
         clock.addObserver(new TwentyFourHour());
 
         new Thread(clock).start();
-
-//        for (int i=0; i<10 ;i++) {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            clock.incSecond();
-//        }
     }
 
 }
